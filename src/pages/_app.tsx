b247df8468981c7bdaf2f743dddcd8976ddb960d
Rodrigo Benedito <rodrigo.carvalho@cadmus.com.br>
2021-06-02 11:20:08 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { AppProps } from 'next/app';
+import { GlobalStyles } from '../styles/global-style';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../styles/theme';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
